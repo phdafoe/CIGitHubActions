22ba3ef5fc9f0ec8a15c3da4146a1df4b79ba50f
phdafoe <phdafoe@gmail.com>
2021-02-15 10:16:21 +0100

    init

---
+++ CIGitHubActions/CIGitHubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIGitHubActionsApp.swift
+//  CIGitHubActions
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 15/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct CIGitHubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
